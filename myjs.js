af15676eb6aadf630cf47943bfcb7c89041bfbc7
Marcc Atayde <marcc.atayde@gmail.com>
2020-09-13 15:20:52 +0400

    joseph09

--- myjs.js
+++ myjs.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-   });
+
    setTimeout(function(){
      $(".eapp-whatsapp-chat-root-layout-component a").remove();
      $(".eapps-widget-toolbar-panel-wrapper").remove();
@@ -8,3 +7,14 @@ $(document).ready(function() {
      $(".yottie-widget-inner a").removeAttr("href");
      $(".yottie-widget-inner a").value(" ");
  }, 10000);
+  $(document).ready(function() {
+    $('#teamS1').on('click', function () {
+       $(".showDesc").slideToggle('slow');
+      });
+  });
+  window.onload = function() {
+    Particles.init
+  ({      	selector: '.background',
+  color: ['#DA0463', '#404B69', '#DBEDF3'],
+  connectParticles: true   });
+  };
